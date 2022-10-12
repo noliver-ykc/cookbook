c8fd88b95ef0dbc4b3651dea00adae893dbb7cf2
noliver-ykc <74501096+noliver-ykc@users.noreply.github.com>
2022-10-12 18:17:31 +0900

    Update Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,32 +1,34 @@
-import React, { useContext } from 'react'
-import IngredientList from './IngredientList'
-import { RecipeContext } from './App'
+import React, { useContext } from "react";
+import IngredientList from "./IngredientList";
+import { RecipeContext } from "./App";
 
 export default function Recipe(props) {
   // using destructuring on line 3 will crowd the text, so placing in a variable here
-  const { handleRecipeDelete, handleRecipeSelect} = useContext(RecipeContext)
-  const {
-    id,
-    name,
-    cookTime,
-    servings,
-    instructions,
-    ingredients
-  } = props
+  const { 
+    handleRecipeDelete, 
+    handleRecipeSelect 
+  } = useContext(RecipeContext);
+  const { id,
+         name,
+         cookTime,
+         servings,
+         instructions,
+         ingredients 
+        } = props;
 
   return (
-    <div className='recipe'>
+    <div className="recipe">
       <div className="recipe-header">
-        <h3 className='recipe-title'>{name}</h3>
+        <h3 className="recipe-title">{name}</h3>
         <div className="btns">
           <button
-            className='btn btn--primary mr-1'
-            onClick={()=> handleRecipeSelect(id)}
+            className="btn btn--primary mr-1"
+            onClick={() => handleRecipeSelect(id)}
           >
             Edit
           </button>
           <button
-            className='btn btn--danger'
+            className="btn btn--danger"
             onClick={() => handleRecipeDelete(id)}
           >
             Delete
@@ -34,25 +36,25 @@ export default function Recipe(props) {
         </div>
       </div>
       <div className="recipe_row">
-        <span className='recipe-label'>Cook Time:</span>
-        <span className='recipe-value'>{cookTime}</span>
+        <span className="recipe-label">Cook Time:</span>
+        <span className="recipe-value">{cookTime}</span>
       </div>
       <div className="recipe_row">
-        <span className='recipe-label'>Servings:</span>
-        <span className='recipe-value'>{servings}</span>
+        <span className="recipe-label">Servings:</span>
+        <span className="recipe-value">{servings}</span>
       </div>
       <div className="recipe_row">
-        <span className='recipe-label'>Instructions:</span>
-        <div className='recipe-value recipe-instructions recipe-value-indent'>
+        <span className="recipe-label">Instructions:</span>
+        <div className="recipe-value recipe-instructions recipe-value-indent">
           {instructions}
         </div>
       </div>
       <div className="recipe_row">
-        <span className='recipe-label'>Ingredients:</span>
-        <div className='recipe-value  recipe-value-indent'>
-          <IngredientList ingredients={ingredients}/>
+        <span className="recipe-label">Ingredients:</span>
+        <div className="recipe-value  recipe-value-indent">
+          <IngredientList ingredients={ingredients} />
         </div>
       </div>
     </div>
-  )
+  );
 }
